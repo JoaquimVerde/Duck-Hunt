db63833866264ebb25098f34e95f3bd6417e5490
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-02-28 14:39:17 +0000

    starting calculations

--- js/script.js
+++ js/script.js
@@ -1,32 +1,69 @@
-const duckContainer = document.querySelector(".duck-container");
+const game = document.querySelector(".game");
+const duckContainer = document.createElement("div");
+const duck = document.createElement("div");
+duckContainer.classList.add("duck-container");
+duck.classList.add("duck");
+duckContainer.appendChild(duck);
+game.appendChild(duckContainer);
 
-let posX = -500;
-let posY = 25;
+let posX;
+let posY;
+let currentX;
+let currentY;
+let maxX = convertVwToPx(95, 95);
+let maxY = convertVhToPx(99, 99);
+let minX = convertVwToPx(1, 1);
+let minY = convertVhToPx(40, 40);
+let directionX = Math.random() < 0.5 ? -1 : 1;
+let directionY = Math.random() < 0.5 ? -1 : 1;
+
+/* window.addEventListener("resize", function() {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    initalPosition();
+}); */
 
 function initalPosition() {
-    duckContainer.style.bottom = "25%";
-    duckContainer.style.left = "50%";
+    const posVW = currentX = convertVwToPx(25, 75);
+    const posVH = currentY = convertVhToPx(33, 33);
+    duckContainer.style.bottom = posVH + "px";
+    duckContainer.style.left = posVW + "px";
 };
 
-const moveDuck = [
-    { transform: "translate(0, 0)" },
-    { transform: "translate("+ posX +"px, "+ posX  +"px)" }
-];
+initalPosition();
 
-const moveAgain = [
-    { transform: "translate("+ posX +"px, "+ posX +"px)" },
-    { transform: "translate("+ posY +"px, "+ posY +"px)" }
+function positionX(){
+    posX = convertVhToPx(40, 99) - currentX;
+    return posX;
+}
+
+positionX();
+
+function positionY(){
+    posY = convertVwToPx(1, 95) - currentY;
+    return posY;
+}
+
+positionY();
+
+const firstFlight = [
+    { transform: "translate("+ posX + "px, "+ posY + "px)" }
 ];
 
 const duration = {
-    duration: 500,
-    easing: "linear",
+    duration: 1000,
+    easing: "ease-in-out",
     fill: "forwards"
 }
 
+duckContainer.animate(firstFlight, duration);
 
-window.onload = initalPosition;
+function convertVhToPx(min, max) {
+    const random = Math.floor(Math.random() * (max - min) + min);
+    return (random * game.offsetHeight) / 100;
+}
 
-duckContainer.animate(moveDuck, duration).onfinish = () => {
-  duckContainer.animate(moveAgain, duration);
+function convertVwToPx(min, max) {
+    const random = Math.floor(Math.random() * (max - min) + min);
+    return (random * game.offsetWidth) / 100;
 }

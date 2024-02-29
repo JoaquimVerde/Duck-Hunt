801b59d471eae00636a5ada2a9b70c0c5dc8f6ec
andrenunes3 <152313579+andrenunes3@users.noreply.github.com>
2024-02-29 12:12:20 +0000

    background with some js interations

---
+++ js/background.js
@@ -0,0 +1,36 @@
+let hitDuck1= document.getElementById('duckHitBar1');
+let numOfBulletsIma = document.getElementById('numOfBullets');
+let numOfBullets = 5;
+
+
+
+
+
+const paintRed = () => {
+    hitDuck1.style.background = "url(/resources/sprites/scoreImages/hit/duckred.png)";
+    hitDuck1.style.backgroundSize = "cover";
+
+}
+window.onload = () => {
+   
+    
+}
+
+const changeNumOfBullets = (num) => {
+    numOfBulletsIma.style.background = "url(/resources/sprites/scoreImages/shot/shot"+num+".png)";
+    numOfBulletsIma.style.backgroundSize = "cover";
+}
+let changeNumOfBulletsWithClick = () => {
+    if(numOfBullets >= 0){
+     changeNumOfBullets(numOfBullets--);
+    }
+}
+
+document.addEventListener('click',() => changeNumOfBulletsWithClick());
+document.addEventListener('click',() => paintRed());
+
+
+
+
+
+
